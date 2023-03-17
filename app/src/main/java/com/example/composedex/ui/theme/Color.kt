07a2aa76872a804a48677fb38b7e5b2b0043f5e1
Luis Santini <dupradosantini@opus-software.com.br>
2023-03-17 14:50:03 -0300

    Defining color scheme and using accompanist to change system bar color

--- app/src/main/java/com/example/composedex/ui/theme/Color.kt
+++ app/src/main/java/com/example/composedex/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package com.example.composedex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val Red400 = Color(0xFFEF5350)
+val Red700 = Color(0xFFD32E2F)
+val Grey200 = Color(0xFFEFF1F0)
+val Grey400 = Color(0xFFBFC1C0)
+val Grey600 = Color(0xFF787978)
+val Grey800 = Color(0xFF444645)
--- app/src/main/java/com/example/composedex/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composedex/ui/theme/Theme.kt
@@ -5,17 +5,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red700,
+    secondary = Grey600,
+    background = Grey800,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red400,
+    secondary = Grey400,
+    background = Grey200,
 
     /* Other default colors to override
     background = Color.White,
@@ -29,11 +30,16 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun ComposeDexTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
+
+    systemUiController.setSystemBarsColor(
+        color = colors.primary
+    )
 
     MaterialTheme(
         colors = colors,
